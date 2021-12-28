706772fa22b71a62ba9d1a9eab5aed8c9b34c580
Dong Quang Quy <quangquya2@gmail.com>
2021-12-28 16:25:08 +0700

    fix

--- server/app/controllers/messageController.js
+++ server/app/controllers/messageController.js
@@ -5,6 +5,7 @@ const User = require('../models/User');
 const messageController = {
     send: async (req, res) => {
         try {
+            console.log();
             const user = req.user;
             const { content, reply } = req.body;
             const { to } = req.params;
